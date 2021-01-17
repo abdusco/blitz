3f330bddca9db13def006c1a42fb168680e0373e
Abdussamet Kocak <sssmmt@gmail.com>
2021-01-18 01:24:34 +0300

    [ui] Increase padding at the page bottom

--- Blitz.Web/ClientApp/src/App.vue
+++ Blitz.Web/ClientApp/src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <main id="app" ref="app">
-    <div>
-      <div v-if="$route.path !== '/'"
-           class="is-flex is-align-items-center is-justify-content-space-around container">
-        <h1 class="title m-0 is-4">
-          <router-link to="/" class="has-text-warning">🗲 blitz</router-link>
-        </h1>
-        <span class="spacer"></span>
-        <nav class="navbar">
-          <nav-link to="/projects">projects</nav-link>
-          <nav-link to="/cronjobs">cronjobs</nav-link>
-          <nav-link to="/executions">executions</nav-link>
-        </nav>
-        <span class="spacer"></span>
-      </div>
-      <router-view/>
+    <div v-if="$route.path !== '/'"
+         class="is-flex is-align-items-center is-justify-content-space-around container">
+      <h1 class="title m-0 is-4">
+        <router-link to="/" class="has-text-warning">🗲 blitz</router-link>
+      </h1>
+      <span class="spacer"></span>
+      <nav class="navbar">
+        <nav-link to="/projects">projects</nav-link>
+        <nav-link to="/cronjobs">cronjobs</nav-link>
+        <nav-link to="/executions">executions</nav-link>
+      </nav>
+      <span class="spacer"></span>
     </div>
+    <router-view/>
   </main>
 </template>
 
@@ -34,4 +32,7 @@ export default {
 </script>
 
 <style>
+#app {
+  padding-bottom: 8rem;
+}
 </style>
