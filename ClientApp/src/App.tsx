0d0623c2ccaef9469e9d79ac08fc6fd1147f8e2b
Abdussamet Kocak <abdus@abdus.dev>
2021-02-27 21:22:56 +0300

    Deduplicate error toasts

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AuthOptions, AuthProvider, useAuth } from './lib/auth';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
@@ -8,13 +8,118 @@ import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import { routes } from './routes';
 import { ChakraProvider, CircularProgress, extendTheme, useToast } from '@chakra-ui/react';
 
+export default function App() {
+    return (
+        <HelmetProvider>
+            <Router>
+                {/* auth provider needs useHistory */}
+                <AuthProvider options={authOptions}>
+                    <QueryClientProvider client={queryClient}>
+                        <ChakraProvider theme={theme} resetCSS={false}>
+                            <LoadingApp>
+                                <FailedQueryNotifier>
+                                    <Switch>
+                                        {/* attach key prop to stop react from complaining */}
+                                        {routes.map((it, i) => React.cloneElement(it, { ...it.props, key: i }))}
+                                    </Switch>
+                                </FailedQueryNotifier>
+                            </LoadingApp>
+                        </ChakraProvider>
+                    </QueryClientProvider>
+                </AuthProvider>
+            </Router>
+        </HelmetProvider>
+    );
+}
+
+const FailedQueryNotifier: React.FC = (props) => {
+    const toast = useToast();
+    const prevRequestConfig = useRef<AxiosRequestConfig>();
+
+    const sameRequest = (current: AxiosRequestConfig, prev: AxiosRequestConfig | undefined): boolean => {
+        return current?.url === prev?.url;
+    };
+
+    useEffect(() => {
+        axios.interceptors.response.use(
+            (val) => val,
+            (err: AxiosError) => {
+                if (sameRequest(err.config, prevRequestConfig.current)) {
+                    return;
+                }
+                
+                prevRequestConfig.current = err.config;
+
+                const isNetworkError = err?.message === 'Network Error';
+                const statusCode = err?.response?.status;
+
+                const title = isNetworkError
+                    ? 'Network error'
+                    : statusCode
+                    ? `Request failed with ${statusCode}`
+                    : 'Request failed';
+                toast({
+                    title: title,
+                    status: 'error',
+                    duration: 2000,
+                    description: (
+                        <div>
+                            Failed to fetch <code>{err.config.url}.</code>
+                            {isNetworkError && (
+                                <>
+                                    <br />
+                                    Make sure you're online and the API can receive requests
+                                </>
+                            )}
+                        </div>
+                    ),
+                });
+            }
+        );
+    }, []);
+    return <>{props.children}</>;
+};
+
+const LoadingApp: React.FC<{ timeout?: number }> = (props) => {
+    const { children, timeout = 500 } = props;
+    const { ready } = useAuth();
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        let id = 0;
+        if (ready) {
+            id = setTimeout(() => setLoaded(true), timeout);
+        }
+
+        return () => clearTimeout(id);
+    }, [ready, setLoaded]);
+
+    if (!loaded) {
+        return (
+            <div
+                style={{
+                    minHeight: '100vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <CircularProgress isIndeterminate size={20} color="purple.500" />
+            </div>
+        );
+    }
+
+    return <>{children}</>;
+};
+
 axios.defaults.baseURL = 'https://localhost:5001/api';
 
 const authOptions: AuthOptions = {
-    authority: 'https://devauth.thyteknik.com.tr',
+    authority: 'https://localhost:5001',
     clientId: 'demoapp',
-    scope: 'openid',
+    scope: 'openid profile',
     redirectUri: 'http://localhost:3000/',
+
     async onUser(user: User | null): Promise<void> {
         if (user) {
             axios.defaults.headers['Authorization'] = `Bearer ${user?.access_token}`;
@@ -79,95 +184,3 @@ const theme = extendTheme({
         },
     },
 });
-
-export default function App() {
-    return (
-        <HelmetProvider>
-            <Router>
-                {/* auth provider needs useHistory */}
-                <AuthProvider options={authOptions}>
-                    <QueryClientProvider client={queryClient}>
-                        <ChakraProvider theme={theme} resetCSS={false}>
-                            <LoadingApp>
-                                <FailedQueryNotifier>
-                                    <Switch>
-                                        {/* attach key prop to stop react from complaining */}
-                                        {routes.map((it, i) => React.cloneElement(it, { ...it.props, key: i }))}
-                                    </Switch>
-                                </FailedQueryNotifier>
-                            </LoadingApp>
-                        </ChakraProvider>
-                    </QueryClientProvider>
-                </AuthProvider>
-            </Router>
-        </HelmetProvider>
-    );
-}
-
-const FailedQueryNotifier: React.FC = (props) => {
-    const toast = useToast();
-    useEffect(() => {
-        axios.interceptors.response.use(
-            (val) => val,
-            (err: AxiosError) => {
-                const isNetworkError = err?.message === 'Network Error';
-                const statusCode = err?.response?.status;
-
-                const title = isNetworkError
-                    ? 'Network error'
-                    : statusCode
-                    ? `Request failed with ${statusCode}`
-                    : 'Request failed';
-                toast({
-                    title: title,
-                    status: 'error',
-                    duration: 2000,
-                    description: (
-                        <div>
-                            Failed to fetch <code>{err.config.url}.</code>
-                            {isNetworkError && (
-                                <>
-                                    <br />
-                                    Make sure you're online and the API can receive requests
-                                </>
-                            )}
-                        </div>
-                    ),
-                });
-            }
-        );
-    }, []);
-    return <>{props.children}</>;
-};
-
-const LoadingApp: React.FC<{ timeout?: number }> = (props) => {
-    const { children, timeout = 500 } = props;
-    const { ready } = useAuth();
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        let id = 0;
-        if (ready) {
-            id = setTimeout(() => setLoaded(true), timeout);
-        }
-
-        return () => clearTimeout(id);
-    }, [ready, setLoaded]);
-
-    if (!loaded) {
-        return (
-            <div
-                style={{
-                    minHeight: '100vh',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <CircularProgress isIndeterminate size={20} color="purple.500" />
-            </div>
-        );
-    }
-
-    return <>{children}</>;
-};
