ff6ed47ad6b236021a3e853871b55707efeec66b
Abdussamet Kocak <abdus@abdus.dev>
2021-06-15 08:54:31 +0300

    fix(api): Remove unused properties

--- Blitz.Web/Cronjobs/TokenAuth.cs
+++ Blitz.Web/Cronjobs/TokenAuth.cs
@@ -1,10 +1,4 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using IdentityModel;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Net.Http.Headers;
-using JwtConstants = Microsoft.IdentityModel.JsonWebTokens.JwtConstants;
-
-namespace Blitz.Web.Cronjobs
+﻿namespace Blitz.Web.Cronjobs
 {
     public class TokenAuth
     {
@@ -12,8 +6,5 @@ namespace Blitz.Web.Cronjobs
         public string Scopes { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string GrantType { get; set; }
-        public string Header { get; set; } = "Authorization";
-        public string AuthScheme { get; set; } = "Bearer";
     }
 }
